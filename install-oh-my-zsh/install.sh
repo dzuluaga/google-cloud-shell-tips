f1f56ca892fe6e50fe96fd9d1f5d28cb1df0dde2
Diego Zuluaga <dzuluaga@apigee.com>
2018-08-03 10:08:27 -0700

    Update install.sh

--- install-oh-my-zsh/install.sh
+++ install-oh-my-zsh/install.sh
@@ -3,3 +3,5 @@ sudo apt-get upgrade --yes
 sudo apt-get install zsh --yes
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+chsh -s $(which zsh)
